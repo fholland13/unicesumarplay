b48140baa6a2fb1fef41d7d098472f34692f8ffc
Fred Holland <freder.holland@gmail.com>
2017-02-28 05:44:32 -0300

    Alterações no css, js e index.html

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
 
-	window.addEventListener("orientationchange", function(){
-		screen.lockOrientation('portrait');
-	});
-
 	document.addEventListener('deviceready', onDeviceReady, false);
 });
 
@@ -11,19 +7,30 @@ function onDeviceReady(){
 
 	//para o canal
 	var channel = "Cesumaroficial";
+
+	// limpa o chace do localStorage
+	if(localStorage.getItem('maxresults') != 10){
+		//alert(localStorage.getItem('maxresults'));
+		localStorage.setItem('maxresults', 10);
+		localStorage.setItem('channel', channel);
+	}
+
+	//mostra o texto de publicações
+	textoTopo();
 	//funcao para pegar a playlist
 	getPlayList(channel);
 
 	$(document).on('click', '#vidList li', function(){
 		showVideo($(this).attr('videoid'));
+		$('html, body').animate({
+			scrollTop: $("#main").offset().top
+		}, 500);
 	});
 
 	$('#saveOptions').click(function(){
 		saveOptions();
-	});
-
-	$('#clearChannel').click(function(){
-		clearChannel();
+		$('#textoTopo').html('');
+		textoTopo();
 	});
 }
 
@@ -49,8 +56,6 @@ function getPlayList(channel, maxResults = 10){
 				console.log(item);
 				//cria uma variavel playlistId para armazenar todos os videos
 				playlistId = item.contentDetails.relatedPlaylists.uploads;
-
-				console.log("A quantidade e: "+playlistId.length);
 				//chama a funcao getVideos(), recebendo a variavel playlistId e 
 				//a qtde de videos a ser exibidos
 				getVideos(playlistId, maxResults);
@@ -71,19 +76,28 @@ function getVideos(playlistId, maxResults){
 		}, function(data){
 			var output;
 			$.each(data.items, function(i, item){
+				console.log(item);
 				id = item.snippet.resourceId.videoId;
 				title = item.snippet.title;
+				published = item.snippet.publishedAt;
 				thumb = item.snippet.thumbnails.default.url;
-				$('#vidList').append('<li videoId="'+id+'"><img src="'+thumb+'"><h3>'+title+'</h3></li>');
+				$('#vidList').append('<li videoId="'+id+'"><img src="'+thumb+'"><h3>'+title+'<p>'+converterDataPublicada(published)+'</p></h3></li>');
 				$('#vidList').listview().listview('refresh');
 			});
 		} 
 		);
 }
 
+function textoTopo(){
+	var maxResults = localStorage.getItem('maxresults');
+	var channel = localStorage.getItem('channel');
+	var output = 'Confira as últimas ' + maxResults + ' publicações do canal ' + channel;
+	$('#textoTopo').html(output);
+}
+
 function showVideo(id){
 	console.log('Showing Video ' + id);
-	$('#logo').hide();
+	$('#logo').remove();
 	var output = '<iframe width="100%" height="250" src="https://www.youtube.com/embed/'+id+'" frameborder="0" allowfullscreen></iframe>';
 	$('#showVideo').html(output);
 }
@@ -105,4 +119,61 @@ function saveOptions(){
 	setMaxResults(maxResults);
 	$('body').pagecontainer('change', '#main', {options});
 	getPlayList(channel, maxResults);
+}
+
+function converterDataPublicada(dataPublicada){
+
+    data = new Date(dataPublicada);
+    ano = data.getFullYear('yyyy');
+    mes = data.getMonth('MMM')+1;
+    dia = data.getDate();
+
+    if(dia < 10){
+        dia = '0' + dia;
+    }
+    switch(mes){
+        case 1:
+        mes = 'jan';
+        break;
+        case 2:
+        mes = 'fev';
+        break;
+        case 3:
+        mes = 'mar';
+        break;
+        case 4:
+        mes = 'abr';
+        break;
+        case 5:
+        mes = 'mai';
+        break;
+        case 6:
+        mes = 'jun';
+        break;
+        case 7:
+        mes = 'jul';
+        break;
+        case 8:
+        mes = 'ago';
+        break;
+        case 9:
+        mes = 'set';
+        break;
+        case 10:
+        mes = 'out';
+        break;
+        case 11:
+        mes = 'nov';
+        break;
+        case 12:
+        mes = 'dez';
+        break;
+    	default:
+        mes = ''
+    }
+    
+	dataPulicadaCompleta = "Publicado em " + dia + ' de ' + mes + ' de ' + ano;
+    
+    return(dataPulicadaCompleta);
+
 }
